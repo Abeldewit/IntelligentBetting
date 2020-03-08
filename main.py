82d05a4708360b489ba03219c3bf1d6415e870db
MartijnHilders <martijnhilders@live.nl>
2020-03-08 14:36:08 +0100

    data added and ignored
    add them yourself https://www.kaggle.com/rounakbanik/the-movies-dataset

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-from sportmonks.soccer import SoccerApiV2
-
-TOKEN = 'H3KQigPBXehIQBrVn691GOgEPPiJUSnDvFEPh7lfIoha7ud5OxQRX0PAYBJB'
-
-# type in de python console: "pip install sportmonks" en daarna "pip install tzlocal"
-
 def main():
-    soccer = SoccerApiV2(api_token=TOKEN)
-    fixtures = soccer.fixtures_today(includes=('localTeam', 'visitorTeam'))
-
-    for f in fixtures:
-        print(f['localTeam']['name'], 'plays at home against', f['visitorTeam']['name'])
-
+	return 0
 
 
 if __name__ == '__main__':
