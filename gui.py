91f6dff1fee6a9fbde0a2f306954369891533679
Abel de Wit <abel@detailweb.wireless.unimaas.local>
2020-03-08 14:25:39 +0100

    Dingen

---
+++ gui.py
@@ -0,0 +1,12 @@
+import tkinter
+
+
+def tester():
+    print("Testing GUI")
+    t = tkinter.Tk()
+    t.title("Movie Tinder")
+    
+
+
+if __name__ == '__main__':
+    tester()
--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-from sportmonks.soccer import SoccerApiV2
-
-TOKEN = 'H3KQigPBXehIQBrVn691GOgEPPiJUSnDvFEPh7lfIoha7ud5OxQRX0PAYBJB'
-
-# type in de python console: "pip install sportmonks" en daarna "pip install tzlocal"
-
 def main():
-    soccer = SoccerApiV2(api_token=TOKEN)
+
     
 
 if __name__ == '__main__':
