5d664b32e12db2b78e466562f1ef90a5718faa5e
Abel de Wit <abel@detailweb.wireless.unimaas.local>
2020-03-08 13:35:17 +0100

    dingen

---
+++ main.py
@@ -0,0 +1,11 @@
+from sportmonks.soccer import SoccerApiV2
+
+TOKEN = 'H3KQigPBXehIQBrVn691GOgEPPiJUSnDvFEPh7lfIoha7ud5OxQRX0PAYBJB'
+
+
+def main():
+    soccer = SoccerApiV2(api_token=TOKEN)
+    soccer.
+
+if __name__ == '__main__':
+    main()
