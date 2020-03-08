64eddef3b95b943b3ffb0a1f13941b2d883bc783
MartijnHilders <martijnhilders@live.nl>
2020-03-08 13:49:09 +0100

    comments voor mensen die niet snappen hoe ze het moeten runnen

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from sportmonks.soccer import SoccerApiV2
 
 TOKEN = 'H3KQigPBXehIQBrVn691GOgEPPiJUSnDvFEPh7lfIoha7ud5OxQRX0PAYBJB'
 
+# type in de python console: "pip install sportmonks" en daarna "pip install tzlocal"
 
 def main():
     soccer = SoccerApiV2(api_token=TOKEN)
-    soccer.
+    
 
 if __name__ == '__main__':
     main()
