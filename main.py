793098d4671967d23fa82145742e6ac43f7694f1
MartijnHilders <martijnhilders@live.nl>
2020-03-08 14:24:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ TOKEN = 'H3KQigPBXehIQBrVn691GOgEPPiJUSnDvFEPh7lfIoha7ud5OxQRX0PAYBJB'
 
 def main():
     soccer = SoccerApiV2(api_token=TOKEN)
-    
+    fixtures = soccer.fixtures_today(includes=('localTeam', 'visitorTeam'))
+
+    for f in fixtures:
+        print(f['localTeam']['name'], 'plays at home against', f['visitorTeam']['name'])
+
+
 
 if __name__ == '__main__':
     main()
