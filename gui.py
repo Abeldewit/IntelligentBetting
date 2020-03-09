1841231e75f57637befa9ee26baa2c10c39d8796
Abel de Wit <abel@detailweb.wireless.unimaas.local>
2020-03-09 14:40:46 +0100

    Title added an some size changes

--- gui.py
+++ gui.py
@@ -21,13 +21,20 @@ def get_movie_info():
 class Example(flx.PyWidget):
 
     def init(self):
-        with flx.VSplit():
+        self.main_container = flx.VSplit(style='font-family: "Helvetica Neue";')
+        with self.main_container:
             with flx.VSplit(flex=2):
-                self.image = flx.Widget(flex=2, style='background: black;')
-                self.overview = ui.Label(flex=1, wrap=True)
+                self.image = flx.Widget(flex=4, style='background: black;')
+                self.title_w = flx.Widget(flex=1)
+                with self.title_w:
+                    self.title = ui.Label(wrap=True)
+                self.overview_w = flx.Widget(flex=1, style='height: 10vh;')
+                with self.overview_w:
+                    self.overview = ui.Label(wrap=True)
             with flx.HBox(flex=1):
                 self.h_button = flx.Button(text='Horrible', flex=1)
                 self.m_button = flx.Button(text='Meh', flex=1)
+                self.n_button = flx.Button(text='Not\nSeen', flex=1)
                 self.g_button = flx.Button(text='Good', flex=1)
                 self.a_button = flx.Button(text='Amazing', flex=1)
 
@@ -41,6 +48,9 @@ class Example(flx.PyWidget):
                 'background-size: contain' \
                 .format(information['Poster'][8:])
         self.image.apply_style(style)
+        self.title.set_text(information['Title'])
+        self.title_w.apply_style('font-size: calc(1em + 2vw); font-weight: \'bold\'; text-align: center;')
+        self.overview_w.apply_style('font-size: calc(1em + 0.85vw); text-align: center;')
         self.overview.set_text(information['Plot'])
 
 
